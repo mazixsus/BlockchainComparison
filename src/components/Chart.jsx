11dd065b99163634d804825b5d5b87ace0925c6a
mateusz <mateusz3249@gmail.com>
2024-04-18 11:51:23 +0200

    granularity, compare with errors fixes, slider fix

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { use, useEffect } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { Box } from "@mui/system";
-import { Card, Slider } from "@mui/material";
+import { Card, Hidden, Slider } from "@mui/material";
 import { Select, MenuItem, Typography } from "@mui/material";
 import { Chip } from "@mui/material";
 import { OutlinedInput } from "@mui/material";
@@ -15,20 +15,41 @@ const blockchains = [
   { key: "avalanche", name: "Avalanche" },
   { key: "solana", name: "Solana" },
   { key: "arbitrum", name: "Arbitrum" },
-  { key: "bsc", name: "Binance Smart Chain" },
+  // { key: "bsc", name: "Binance Smart Chain" },
   { key: "ethereum", name: "Ethereum" },
   { key: "polygon", name: "Polygon" },
   { key: "optimism", name: "Optimism" },
   // {key: "alephzero", name: "Aleph Zero"},
 ];
 
-export const Chart = ({ blockChainData, cumulativeData, blockchain, cumulativeBC, setBlockchain, setCumulativeBC }) => {
-  const dates = blockChainData.tg_growth_index.map((item) => item.date);
-  const values = blockChainData.tg_growth_index.map((item) => item.value);
-  const cumulative = cumulativeData.tg_growth_index.map((item) => item.value);
+export const Chart = ({ basicTimelineData, blockchain, cumulativeBC, setBlockchain, setCumulativeBC }) => {
+  const [chartData, setChartData] = React.useState(basicTimelineData.tg_growth_index);
 
-  const [value, setValue] = React.useState([0, dates.length - 1]);
+  const [sliderValue, setSliderValue] = React.useState([0, basicTimelineData.tg_growth_index.dates.length - 1]);
   const minDistance = 10;
+
+  const [granularity, setGranularity] = React.useState("week");
+
+  useEffect(() => {
+    setChartData(basicTimelineData.tg_growth_index);
+    setSliderValue([0, chartData.dates.length - 1]);
+    setGranularity("week");
+  }, [basicTimelineData]);
+
+  useEffect(() => {
+    setSliderValue([0, chartData.dates.length - 1]);
+  }, [chartData]);
+
+  const handleChangeGranularity = (event) => {
+    setGranularity(event.target.value);
+    if (event.target.value === "week") {
+      setChartData(basicTimelineData.tg_growth_index);
+    } else if (event.target.value === "two_weeks") {
+      setChartData(basicTimelineData.avgBiweekly);
+    } else if (event.target.value === "four_weeks") {
+      setChartData(basicTimelineData.avgMonthly);
+    }
+  };
 
   const handleChange = (event, newValue, activeThumb) => {
     if (!Array.isArray(newValue)) {
@@ -37,20 +58,20 @@ export const Chart = ({ blockChainData, cumulativeData, blockchain, cumulativeBC
 
     if (newValue[1] - newValue[0] < minDistance) {
       if (activeThumb === 0) {
-        const clamped = Math.min(newValue[0], 100 - minDistance);
-        setValue([clamped, clamped + minDistance]);
+        const clamped = Math.min(newValue[0], chartData.dates.length - 1 - minDistance);
+        setSliderValue([clamped, clamped + minDistance]);
       } else {
         const clamped = Math.max(newValue[1], minDistance);
-        setValue([clamped - minDistance, clamped]);
+        setSliderValue([clamped - minDistance, clamped]);
       }
     } else {
-      setValue(newValue);
+      setSliderValue(newValue);
     }
   };
 
   return (
     <Box>
-      <Box display={"flex"} alignItems={"center"} my={1}>
+      <Box display={"flex"} alignItems={"center"} my={2}>
         <Typography mr={1}>Blockchain:</Typography>
         <FormControl sx={{ minWidth: 200 }}>
           <InputLabel id="blockchain-lebel" size="small">
@@ -116,28 +137,43 @@ export const Chart = ({ blockChainData, cumulativeData, blockchain, cumulativeBC
           </Select>
         </FormControl>
       </Box>
-
+      <Box display={"flex"} alignItems={"center"} my={2}>
+        <Typography mr={1}>Granularity:</Typography>
+        <FormControl sx={{ minWidth: 200 }}>
+          <InputLabel id="blockchain-lebel" size="small">
+            Choose Granularity
+          </InputLabel>
+          <Select
+            type="text"
+            size="small"
+            labelId="blockchain-lebel"
+            sx={{ mr: 1, background: "rgba(255, 255, 255, 0.3)" }}
+            value={granularity}
+            input={<OutlinedInput label="Choose Granularity" sx={{ background: "rgba(255, 255, 255, 0.3)" }} />}
+            onChange={handleChangeGranularity}
+          >
+            <MenuItem value="week">Week</MenuItem>
+            <MenuItem value="two_weeks">Two Weeks</MenuItem>
+            <MenuItem value="four_weeks">Four Weeks</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
       <Card sx={{ border: 1, borderRadius: 2, p: 5, borderColor: "grey", background: "rgba(255, 255, 255, 0.3)" }}>
         <LineChart
           xAxis={[
             {
               scaleType: "point",
-              data: dates.slice(value[0], value[1] + 1),
-              // tickLabelStyle: {
-              //   angle: 45,
-              //   textAnchor: "start",
-              //   fontSize: 12,
-              // },
+              data: chartData.dates.slice(sliderValue[0], sliderValue[1] + 1),
             },
           ]}
           series={[
             {
-              data: values.slice(value[0], value[1] + 1),
+              data: chartData.blockchainValue.slice(sliderValue[0], sliderValue[1] + 1),
               label: "Growth Index",
             },
             {
-              data: cumulative.slice(value[0], value[1] + 1),
-              label: "Cumulative Growth Index",
+              data: chartData.cumulativeValue.slice(sliderValue[0], sliderValue[1] + 1),
+              ...(chartData.cumulativeValue.length > 0 ? {label: "Cumulative Growth Index"} : { })
             },
           ]}
           fullWidth
@@ -146,15 +182,15 @@ export const Chart = ({ blockChainData, cumulativeData, blockchain, cumulativeBC
         ></LineChart>
         <Box px={5} mt={5}>
           <Slider
-            value={value}
+            value={sliderValue}
             onChange={handleChange}
             min={0}
-            max={dates.length - 1}
-            marks={dates.map((date, index) => {
+            max={chartData.dates.length - 1}
+            marks={chartData.dates.map((date, index) => {
               return { value: index, label: date };
             })}
-            getAriaValueText={(value) => dates[value]}
-            valueLabelFormat={(value) => dates[value]}
+            getAriaValueText={(value) => chartData.dates[value]}
+            valueLabelFormat={(value) => chartData.dates[value]}
             valueLabelDisplay="on"
             sx={{
               mt: 2,
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,9 +5,74 @@ import { Box } from "@mui/system";
 import { Divider, Typography } from "@mui/material";
 import { Backdrop, CircularProgress } from "@mui/material";
 
+function getAverage(data) {
+  let accAvgMonthly = [];
+  let accAvgBiweekly = [];
+
+  const [fy, fm, fd] = data.tg_growth_index[0].date.split("-");
+  let accLastDate = `${fy}-${fm}`;
+  let accLastMonthPart = fd > 15 ? 2 : 1;
+
+  let avgMonthly = [];
+  let avgBiweekly = [];
+  data.tg_growth_index.forEach(({ date, value }, index) => {
+    const [year, month, day] = date.split("-");
+    if (accLastDate !== `${year}-${month}`) {
+      // monthly average
+      avgMonthly.push({ data: accLastDate, value: accAvgMonthly.reduce((acc, x) => acc + x, 0) / accAvgMonthly.length });
+      accAvgMonthly = [];
+      // biweekly average
+      avgBiweekly.push({ data: `II ${accLastDate}`, value: accAvgBiweekly.reduce((acc, x) => acc + x, 0) / accAvgBiweekly.length });
+      accAvgBiweekly = [];
+      accLastMonthPart = 1;
+
+      accLastDate = `${year}-${month}`;
+    } else if (accLastMonthPart === 1 && day > 15) {
+      avgBiweekly.push({ data: `I ${accLastDate}`, value: accAvgBiweekly.reduce((acc, x) => acc + x, 0) / accAvgBiweekly.length });
+      accAvgBiweekly = [];
+      accLastMonthPart = 2;
+    }
+    accAvgMonthly.push(value);
+    accAvgBiweekly.push(value);
+
+    if (index === data.tg_growth_index.length - 1) {
+      avgMonthly.push({ data: accLastDate, value: accAvgMonthly.reduce((acc, x) => acc + x, 0) / accAvgMonthly.length });
+      avgBiweekly.push({ data: `II ${accLastDate}`, value: accAvgBiweekly.reduce((acc, x) => acc + x, 0) / accAvgBiweekly.length });
+    }
+  });
+
+  data["avgMonthly"] = avgMonthly;
+  data["avgBiweekly"] = avgBiweekly;
+}
+
+function transformDatas(data) {
+  let blockchainValue = data.tokenGuardData.blockchain.tg_growth_index.map((item) => item.value);
+  let blockchainAvgMonthly = data.tokenGuardData.blockchain.avgMonthly.map((item) => item.value);
+  let blockchainAvgBiweekly = data.tokenGuardData.blockchain.avgBiweekly.map((item) => item.value);
+
+  let cumulativeValue = [];
+  let cumulativeAvgMonthly = [];
+  let cumulativeAvgBiweekly = [];
+  if (Object.keys(data.tokenGuardData.cumulative).length !== 0) {
+    cumulativeValue = data.tokenGuardData.cumulative.tg_growth_index.map((item) => item.value);
+    cumulativeAvgMonthly = data.tokenGuardData.cumulative.avgMonthly.map((item) => item.value);
+    cumulativeAvgBiweekly = data.tokenGuardData.cumulative.avgBiweekly.map((item) => item.value);
+  }
+
+  let dates = data.tokenGuardData.blockchain.tg_growth_index.map((item) => item.date);
+  let datesAvgMonthly = data.tokenGuardData.blockchain.avgMonthly.map((item) => item.data);
+  let datesAvgBiweekly = data.tokenGuardData.blockchain.avgBiweekly.map((item) => item.data);
+
+  return {
+    tg_growth_index: { blockchainValue, cumulativeValue, dates },
+    avgMonthly: { blockchainValue: blockchainAvgMonthly, cumulativeValue: cumulativeAvgMonthly, dates: datesAvgMonthly },
+    avgBiweekly: { blockchainValue: blockchainAvgBiweekly, cumulativeValue: cumulativeAvgBiweekly, dates: datesAvgBiweekly },
+  };
+}
+
 export const MainPage = () => {
-  const [blockchainData, setBlockchainData] = React.useState([]);
-  const [cumulativeData, setCumulativeData] = React.useState([]);
+  const [basicTimelineData, setBasicTimelineData] = React.useState([]);
+
   const [loading, setLoading] = React.useState(true);
 
   const [blockchain, setBlockchain] = React.useState("near");
@@ -35,9 +100,13 @@ export const MainPage = () => {
       await fetch("/api/basic-timeline-data", requestOptions)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data.tokenGuardData.blockchain);
-          setBlockchainData(data.tokenGuardData.blockchain);
-          setCumulativeData(data.tokenGuardData.cumulative);
+          getAverage(data.tokenGuardData.blockchain);
+          if (Object.keys(data.tokenGuardData.cumulative).length !== 0) getAverage(data.tokenGuardData.cumulative);
+
+          transformDatas(data);
+          console.log(transformDatas(data));
+
+          setBasicTimelineData(transformDatas(data));
           setLoading(false);
         })
         // .then((result) => console.log(result))
@@ -48,10 +117,13 @@ export const MainPage = () => {
   }, [blockchain, cumulativeBC]);
 
   return (
-    <div class="flex h-screen p-4">
+    <div className="flex h-screen p-4">
       <Box className="bg-white/30 backdrop-blur-lg w-full h-full px-10 py-5 rounded-lg">
-        <h1 class="mb-3 text-5xl ">Blockchain Grow Index</h1>
-        <Divider className="mb-6"/>
+        <Box display={"flex"} alignItems={"center"}>
+          <img src="/logo.svg" alt="logo" className="w-20 h-20 mb-5 mr-5" />
+          <h1 className="mb-3 text-5xl ">Blockchain Growth Index</h1>
+        </Box>
+        <Divider className="mb-6" />
         {loading ? (
           <Backdrop
             sx={{
@@ -71,8 +143,7 @@ export const MainPage = () => {
           </Backdrop>
         ) : (
           <Chart
-            blockChainData={blockchainData}
-            cumulativeData={cumulativeData}
+            basicTimelineData={basicTimelineData}
             blockchain={blockchain}
             cumulativeBC={cumulativeBC}
             setBlockchain={setBlockchain}
